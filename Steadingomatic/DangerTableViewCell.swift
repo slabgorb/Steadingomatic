077d74108d6c81c4b20798226639f7e9d54a6145
Keith Avery <slabgorb@gmail.com>
2016-02-26 15:27:33 -0500

    danger view controller stuff

---
+++ Steadingomatic/DangerTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  DangerTableViewCell.swift
+//  Steadingomatic
+//
+//  Created by Keith Avery on 2/23/16.
+//  Copyright © 2016 Keith Avery. All rights reserved.
+//
+
+import UIKit
+
+class DangerTableViewCell: UITableViewCell {
+
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    // MARK: Properties
+
+    @IBOutlet weak var tableDangers: UITableView!
+    @IBOutlet weak var labelName: UILabel!
+
+
+}
---
+++ Steadingomatic/DangerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DangerViewController.swift
+//  Steadingomatic
+//
+//  Created by Keith Avery on 2/22/16.
+//  Copyright © 2016 Keith Avery. All rights reserved.
+//
+
+import UIKit
+
+class DangerViewController: UIViewController {
+    var front: Front?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
